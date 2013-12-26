74a1d6507c39b1a98886e7ccec99b69de7e4c7ec
GoodLuckBamboo <1594717267@qq.com>
2013-12-26 16:00:46 +0800

    new change

--- ch05/README.md
+++ ch05/README.md
@@ -1,5 +1,7 @@
 Linked List
 ========
 
-
-
+linked lists are some of the most fundamental data structures. Linked lists consist
+of a number of elements grouped, or linked, together in a specific order. They are
+useful in maintaining collections of data, similar to the way that arrays are often
+used.
--- ch05/list.c
+++ ch05/list.c
@@ -10,59 +10,42 @@ void list_init(List *list, void (*destroy)(void *data)) {
     return;
 }
 
-
 void list_destroy(List *list) {
-
     void               *data;
     while (list_size(list) > 0) {
-
         if (list_rem_next(list, NULL, (void **)&data) == 0 && list->destroy !=
                 NULL) {
             list->destroy(data);
         }
-
     }
     memset(list, 0, sizeof(List));
     return;
 }
 
-
 int list_ins_next(List *list, ListElmt *element, const void *data) {
-
     ListElmt           *new_element;
-
 
     if ((new_element = (ListElmt *)malloc(sizeof(ListElmt))) == NULL)
         return -1;
 
-
     new_element->data = (void *)data;
 
     if (element == NULL) {
-
-
         if (list_size(list) == 0)
             list->tail = new_element;
 
         new_element->next = list->head;
         list->head = new_element;
-
     }
-
     else {
-
-
         if (element->next == NULL)
             list->tail = new_element;
-
+        
         new_element->next = element->next;
         element->next = new_element;
 
     }
-
-
     list->size++;
-
     return 0;
 
 }
@@ -98,7 +81,6 @@ int list_rem_next(List *list, ListElmt *element, void **data) {
         if (element->next == NULL)
             list->tail = element;
     }
-
     free(old_element);
     list->size--;
     return 0;
